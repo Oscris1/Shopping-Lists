cc0b2ea69424e67a4183859fac40b6f1e47cac01
Damian Piętka <damianpietka@gmail.com>
2021-09-01 22:03:47 +0200

    newListButton has been created

--- screens/ActiveListsScreen.tsx
+++ screens/ActiveListsScreen.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 const ActiveListsScreen = () => {
   return (
     <View style={styles.container}>
       <Text>ActiveListsScreen</Text>
+      <TouchableOpacity
+        style={styles.newListButton}
+        onPress={() => console.log('button pressed')}>
+        <Text style={{color: '#fff', fontSize: 30}}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -13,6 +18,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  newListButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+
+    height: 70,
+    width: 70,
+    borderRadius: 35,
+    backgroundColor: 'tomato',
   },
 });
 
