2460a0169ff8a0a71ab2bd9b0f29909c466eeb22
Damian Piętka <damianpietka@gmail.com>
2021-09-01 21:47:42 +0200

    initial configuration

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
